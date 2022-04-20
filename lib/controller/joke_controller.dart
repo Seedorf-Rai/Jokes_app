1d512b4b92e88d1b1b51d9ba6db13a0f9baf82cd
Seedorf-Rai <dorf123see@gmail.com>
2022-04-20 20:37:09 +0545

    jokeController created

---
+++ lib/controller/joke_controller.dart
@@ -0,0 +1,31 @@
+import 'package:get/get.dart';
+import 'package:jokes_app/model/joke_model.dart';
+import 'package:jokes_app/remote_service/remote_service.dart';
+
+class JokeController extends GetxController {
+  var joke = JokeModel(
+    categories: [],
+    iconUrl: ' ',
+    id: '',
+    url: '',
+    value: '',
+  ).obs;
+  var isLoading = true.obs;
+
+  Future getJokes() async {
+    try {
+      var data = await RemoteService.fetchJokes();
+      if (data != null) {
+        joke.value = data;
+      }
+    } catch (e) {
+      Get.snackbar("Message", e.toString());
+    }
+  }
+
+  @override
+  void onInit() {
+    super.onInit();
+    getJokes();
+  }
+}
